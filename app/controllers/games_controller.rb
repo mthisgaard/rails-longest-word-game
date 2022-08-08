68376c92f73d711531496656951750654847c5cd
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-08 13:53:08 +0900

    add style

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,14 @@
 class GamesController < ApplicationController
 
   def new
+    grid = []
+    10.times do
+      grid << ('a'..'z').to_a.sample
+    end
+    @letters = grid
   end
 
   def score
+    binding.pry
   end
 end
